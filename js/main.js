5b6201a09d60d533694907d967b28d325d7d1ea0
Karthick <karthick@Karthick.local>
2026-02-02 23:31:36 +0530

    Initial commit of Ayira Pantry website

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Mobile Menu Toggle
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuBtn = document.querySelector('.mobile-menu-toggle');
+    const mainNav = document.querySelector('.main-nav');
+
+    if (mobileMenuBtn && mainNav) {
+        mobileMenuBtn.addEventListener('click', () => {
+            mainNav.classList.toggle('active');
+            const icon = mobileMenuBtn.querySelector('i');
+            if (mainNav.classList.contains('active')) {
+                icon.classList.remove('fa-bars');
+                icon.classList.add('fa-times');
+            } else {
+                icon.classList.remove('fa-times');
+                icon.classList.add('fa-bars');
+            }
+        });
+    }
+
+    console.log('Ayira Pantry Loaded');
+});
